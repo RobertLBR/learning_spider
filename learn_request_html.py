a3b293cbf4b3959ddcc1b302ed00c53d66826cea
luborong <luborong@guide-rank.com>
2025-04-23 17:04:49 +0800

    update

--- learn_request_html.py
+++ learn_request_html.py
@@ -24,17 +24,8 @@ if json_data_match:
         price_info = series['price_info']['price']
         online_date_month = series['online_date_month']
         online_date_day = series['online_date_day']
+        print(f"上市时间：{online_date_month}月{online_date_day}日，新车发布：{series_name}，售价范围：{price_info}")
 
-        print(f"新车发布：{series_name}")
-        print(f"售价范围：{price_info}")
-        print(f"上市时间：{online_date_month}月{online_date_day}日")
-
-        for tag in series['tag_list']:
-            tag_type = tag['name']
-            print(f"上新类型：{tag_type}")
-
-        # 分割线
-        print("-" * 20 )
 
 else:
     print("JSON data not found in the HTML.")
