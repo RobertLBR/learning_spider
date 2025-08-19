6fd496df1a78e05ec5aadfc976b4d6ed0d136094
luborong <luborong@guide-rank.com>
2025-08-19 15:07:02 +0800

    更新豆瓣采集程序

--- requests/douban_spider.py
+++ requests/douban_spider.py
@@ -3,63 +3,51 @@
 
 import requests
 import re
-import csv
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from bs4 import BeautifulSoup
 
-#定义页面与请求头信息
-headers = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36 Edg/93.0.961.52"
-}
-
+# 获取页面源码
 def get_page_info(url):
+    # 定义页面与请求头信息
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36 Edg/93.0.961.52"
+    }
+
     #获取页面数据
     resp = requests.get(url,headers=headers).text
-    #print(resp)
-
-    #解析数据
-    obj = re.compile(r'<li>.*?<div class="item">.*?<span class="title">(?P<name>.*?)'
-                     r'</span>.*?<div class="bd">.*?<br>(?P<year>.*?)'
-                     r'&nbsp;/&nbsp;(?P<country>.*?)&nbsp;/&nbsp;(?P<mtype>.*?)'
-                     r'</p>.*?<div class="star">.*?<span class="rating_num" property="v:average">(?P<rat>.*?)'
-                     r'</span>.*?<span>(?P<number>.*?)人评价</span>', re.S)
-
-    results = obj.finditer(resp)
+    status_code = requests.get(url, headers=headers).status_code
+    print(status_code)
+    return resp
 
 
-    for result in results:
-        # 打印方式一
-        print(result.group("year").strip(), result.group("name"), result.group("rat"), result.group("number"))
+# 使用BeautifulSoup提取页面内容
+def soup_data(resp):
+    soup = BeautifulSoup(resp,'lxml')
+    items = soup.find_all('div',class_="item")
 
-        # 转化为字典格式
-        dic = result.groupdict()
-        dic['year'] = dic['year'].strip()
-        dic['mtype'] = dic['mtype'].strip()
+    for item in items:
+        try:
+            # soup提取
+            rank = item.find('em').text
+            title = item.find('span',class_="title").text
+            average = item.find('span',class_="rating_num").text
+            info = item.find('p').text.replace('\n','').replace(' ','')
+            print("排名：",rank,"评分：",average,"电影：",title,"年份：",info.split("...")[1])
+        except Exception as e:
+            print(e)
 
-        # 打印方式二
 
+if __name__ == '__main__':
+    # 爬取豆瓣10页电影信息
+    for i in range(10):
+        page = i * 25
+        url = f'https://movie.douban.com/top250?start={page}&filter='
+        print (f"第{i+1}页")
 
-        # print(dic.values())
+        # 获取页面源码
+        resp = get_page_info(url)
 
-        # 打印方式三
-        # for key in dic.keys():
-        #     print(key,":",dic.get(key))
-        # print("-------------------------")
+        # 使用BS4处理
+        soup_data(resp)
 
-        # 将字典数据写入excel文件
-        # with open('douban_top.csv', 'a') as f:
-        #     csvwrite=csv.writer(f) #定义csv写人
-        #     csvwrite.writerow(dic.values())
-
-# 爬取豆瓣10页电影信息
-for i in range(10):
-    page = i * 25
-    url = f'https://movie.douban.com/top250?start={page}&filter='
-    # print (url)
-    # print (f"第{i}页")
-    get_page_info(url)
-
-# with ThreadPoolExecutor(10) as t:
-#    for i in range(10):
-#         page = i * 25
-#         url = f'https://movie.douban.com/top250?start={page}&filter='
-#         t.submit(get_page_info, url=url)
+        # 测试
+        # break
